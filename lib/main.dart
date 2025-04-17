72f8b01a5938eeddc35f84a3fca9dbb9232e97b8
MalithKeerthisinghe <102132303+MalithKeerthisinghe@users.noreply.github.com>
2025-04-17 10:36:19 +0530

    ignored unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:investment_plan_app/services/user_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
